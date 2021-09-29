2ebe419c53e9c43b0e3f01ba0d6a276174a73151
AviTS <avisabath@gmail.com>
2021-09-29 09:37:17 -0400

    initial commit

---
+++ makeText.js
@@ -0,0 +1,5 @@
+const markov = require('./markov')
+const axios = require('axios')
+const process = require('process')
+const fs = require('fs')
+
---
+++ markov.js
@@ -0,0 +1,54 @@
+/** Textual markov chain generator */
+
+
+class MarkovMachine {
+
+  /** build markov machine; read in text.*/
+
+  constructor(text) {
+    let words = text.split(/[ \r\n]+/);
+    this.words = words.filter(c => c !== "");
+    this.makeChains();
+  }
+
+  /** set markov chains:
+   *
+   *  for text of "the cat in the hat", chains will be
+   *  {"the": ["cat", "hat"], "cat": ["in"], "in": ["the"], "hat": [null]} */
+
+  makeChains() {
+    let mChains = new Map();
+
+    for (let i=0; i<this.words.length; i++) {
+      let word = this.words[i];
+      let nextWord = this.words[i+1] || null;
+
+      if (mChains.has(word)) mChains.get(word).push(nextWord);
+      else mChains.set(word, [nextWord]);
+    }
+    this.mChains = mChains;
+  }
+
+  static choice(ar) {
+    return ar[Math.floor(Math.random() * ar.length)];
+  }
+
+  /** return random text from chains */
+
+  makeText(numWords = 100) {
+    let keys = Array.from(this.mChains.keys())
+    let key = MarkovMachine.choice(keys)
+    let output = [];
+
+    while (output.length < numWords && key !== null) {
+      out.push(key);
+      key = MarkovMachine.choice(this.chains.get(key));
+    }
+
+    return out.join(" ");
+  }
+}
+
+module.exports = {
+  MarkovMachine,
+};
